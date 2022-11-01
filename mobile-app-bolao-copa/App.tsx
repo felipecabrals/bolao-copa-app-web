4a7e0c6c8a8720b98bbc1c0244e0fb373a27ba29
Felipe Dias <flp.dsc@gmail.com>
2022-11-01 16:08:09 -0300

    customização do nativebase, install fonts personalizadas, criando o loading e signin

--- mobile-app-bolao-copa/App.tsx
+++ mobile-app-bolao-copa/App.tsx
@@ -1,25 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { } from 'expo-status-bar';
+import { NativeBaseProvider, StatusBar } from "native-base";
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { THEME } from './src/styles/theme';
 
-import { NativeBaseProvider, Center } from "native-base";
-import { THEME } from './src/styles/theme'
+import { Loading } from './src/components/Loading';
+import { SignIn } from './src/screens/SignIn';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold })
   return (
     <NativeBaseProvider theme={THEME}>
-      <Center flex={1} bgColor={"gray.900"}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </Center>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
